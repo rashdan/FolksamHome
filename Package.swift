5b1a2ab69cf91a698aa4d9c27e224f3e5d9f6034
Rashdan Natiq <rashdan@natiq.com>
2022-03-08 10:04:07 +0100

    Updated package version

--- Package.swift
+++ Package.swift
@@ -21,10 +21,12 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "FolksamCommon",
-                     url: "https://rashdan@bitbucket.org/folksamfuturelab/folksamcommon.git", .upToNextMajor(from: "0.1.1")),
+                     url: "https://github.com/rashdan/FolksamCommon.git", .upToNextMajor(from: "0.1.2")),
         .package(url: "https://github.com/ReSwift/ReSwift.git", .upToNextMajor(from: "6.1.0")),
         .package(
             url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.9.0")
+        
+        //https://rashdan@bitbucket.org/folksamfuturelab/folksamcommon.git
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
