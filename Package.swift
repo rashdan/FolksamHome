43ab04c2797d8f6d98a4bd35c097cc1fa2bd6541
Rashdan Natiq <rashdan@natiq.com>
2022-03-08 11:32:47 +0100

    Updated FolksamCommon package version

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(name: "FolksamCommon",
-                     url: "https://github.com/rashdan/FolksamCommon.git", .upToNextMajor(from: "0.1.2")),
+                     url: "https://github.com/rashdan/FolksamCommon.git", .upToNextMajor(from: "0.1.3")),
         .package(url: "https://github.com/ReSwift/ReSwift.git", .upToNextMajor(from: "6.1.0")),
         .package(
             url: "https://github.com/pointfreeco/swift-snapshot-testing.git", from: "1.9.0")
